1be478ab0f0af54a94ed8e4025ba81639fc546f3
Artem Ablitsov <artem.ablitsov@gmail.com>
2021-11-16 07:19:18 +0300

    Improved Python algorithm

--- python/nervousmouse.py
+++ python/nervousmouse.py
@@ -3,16 +3,28 @@ import math
 import time
 from random import randrange
 
-
+moves = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
+w,h = pyautogui.size()
+radius_big = 3.0*h/(4.0 * 2.0)
+radius_small = radius_big/10
+startx, starty = w/2,h/2
+pos = 0
 while (True):
-    startx, starty = pyautogui.position()    
-    radius = 300
-    for i in range(-180,180,10):        
-        angle = float(i) * math.pi / 180
-        x = int(radius * math.cos(angle))
-        y = int(radius * math.sin(angle))
-        pyautogui.moveTo(startx + x, starty + y)
-        # time.sleep(0.02)
-    rnd = randrange(-20, 20)
-    pyautogui.scroll(rnd, x=startx, y=starty)
-    time.sleep(15)
+    for iouter in range(-180,180,10):        
+        angleouter = float(iouter) * math.pi / 180
+        xcenter = int(radius_big * math.cos(angleouter))
+        ycenter = int(radius_big * math.sin(angleouter))
+        startxcenter, startycenter = w/2 + xcenter,h/2 + ycenter
+        for iinner in range(-180,180,10):        
+            angleinner = float(iinner) * math.pi / 180
+            x = int(radius_small * math.cos(angleinner))
+            y = int(radius_small * math.sin(angleinner))
+            pyautogui.moveTo(startxcenter + x, startycenter + y)
+        pyautogui.moveTo(startx, starty)
+        pyautogui.scroll(moves[pos], x=startx, y=starty)
+        time.sleep(10)
+        if pos >= len(moves) - 1:
+            pos = 0
+        else:
+            pos = pos + 1
+
